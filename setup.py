8dfd7612ca18f55e39097f7cf8339a6bb8cc0721
Yannick Allusse <yallusse@ltutech.com>
2016-06-07 13:14:23 +0200

    specify dependencies versions

--- setup.py
+++ setup.py
@@ -16,7 +16,9 @@ PARAMETERS = {
     'ltu.engine': 'ltu/engine',
     'ltu': 'ltu'
   },
-  'install_requires': ['unittest2', 'mock', 'requests==2.10.0'],
+  'install_requires': ['unittest2==1.1.0',
+                       'mock==2.0.0',
+                       'requests==2.10.0'],
   'author': "LTU technologies",
   'author_email': "support@ltutech.com",
   'maintainer': "LTU technologies",
